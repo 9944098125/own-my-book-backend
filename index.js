ef78217a10cc6b74ae26bd7790cfd41d0e17d5c0
Srinivas Akella <srinivas.prasad@teknotrait.com>
2023-03-22 12:38:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+async function connectDB() {
+  try {
+    await mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Successfully connected to MongoDB Data Base");
+  } catch (err) {
+    throw err;
+  }
+}
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB Data Base disconnecting...");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB Data Base connecting...");
+});
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`App is now running on ${port}`);
+});
